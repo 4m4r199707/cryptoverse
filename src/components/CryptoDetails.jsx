38b2d9fc6b52b9532ab56cda4f051b4003cbef2b
4m4r199707 <4m4rarts@gmail.com>
2021-11-15 16:48:18 +0530

    Line chart and responsible

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -5,7 +5,9 @@ import millify from 'millify';
 import {Col, Row, Typography, Select} from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, NumberOutlined, ThunderboltOutlined, CheckOutlined } from '@ant-design/icons';
 
-import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+
+import LineChart from './LineChart';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -15,7 +17,9 @@ const CryptoDetails = () => {
     const { coinId } = useParams();
     const [ timePeriod, setTimePeriod ] = useState('7d');
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+    const { data:coinHistory} = useGetCryptoHistoryQuery({coinId,timePeriod})
     const cryptoDetails = data?.data?.coin;
+
 
     if(isFetching) return 'Loading...';
 
@@ -54,7 +58,7 @@ const CryptoDetails = () => {
                     onChange={(value) => setTimePeriod(value)}>
                         {time.map((date) => <Option key={date}>{date}</Option>)}
                     </Select>
-
+                    <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name}/>
                     <Col className="stats-container">
                         <Col className="coin-value-statistics">
                             <Col className="coin-value-statistics-heading">
