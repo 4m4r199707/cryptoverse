4bc742c138c4f3d1fcc0745800110093ae3e4124
4m4r199707 <4m4rarts@gmail.com>
2021-11-18 21:50:34 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ const App = () => {
                 <div className="footer">
                 <Typography.Title level={5} style={{color: 'white', textAlign:'center'}}>
                     Cryptoverse <br />
-                    All rights reserved
+                    All rights reserved<br />
+                    App by Amar
                 </Typography.Title>
                 <space>
                     <Link to='/'>Home</Link>
